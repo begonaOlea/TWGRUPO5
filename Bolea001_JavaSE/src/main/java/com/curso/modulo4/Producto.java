1360bd95d59a40d08bc2db8904c50480445185e5
begonaOlea <bolea@programia.es>
2022-10-05 12:00:12 +0200

    Ejemplo construtores this()

--- Bolea001_JavaSE/src/main/java/com/curso/modulo4/Producto.java
+++ Bolea001_JavaSE/src/main/java/com/curso/modulo4/Producto.java
@@ -2,7 +2,8 @@ package com.curso.modulo4;
 
 public class Producto {
 	
-	//atributos
+	//atributos 
+	//variables de instancia
 	private int id; //= 0
 	private String descripcion; // =null
 	private double precio; // = 0.0
@@ -11,14 +12,45 @@ public class Producto {
 	//final - constante
 	public static final double PRECIO_MINIMO = 1.2;
 	
-	public Producto(String s) {
-		this.descripcion = s;
-		this.precio = PRECIO_MINIMO;
+	
+	
+	//constructor
+	public Producto(String s, double precio) {
+		if ( s != null) {
+			this.descripcion = s;
+		}else {
+			this.descripcion = "";// "INDEFINIDO";
+		}
+		if(precio > 0) {
+			this.precio = precio;
+		}
 	}
+		
+	public Producto(int id, String descripcion, double precio) {		
+		this(descripcion, precio); // llama al constructo Producto(String s) 
+		this.id = id;		
+	}
+	
+	public Producto(int id, String descripcion) {
+		this(id,descripcion, PRECIO_MINIMO);
+	}
+	
+	
+	
 	
 	public void incrementarPrecio() {
 		int incremento =2;
 		this.precio += incremento/200;
 	}
 
+	public void prueba() {
+		int x = (int)(Math.random() * 100);
+		int y;
+		int z;
+		if (x > 50) {
+			y = 9;
+		}
+		z = y + x; 
+	}
+	
 }
